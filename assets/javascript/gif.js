1125217fe1dc5dba1b418b189f517070c981aa75
Zika Chea <jallsadou@gmail.com>
2018-07-14 00:48:32 -0500

    Commit Messages

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -4,7 +4,7 @@
 
 
 
-var topics = ["kaka", "ronaldo","messi","hazard","zidane", "iniesta", "cafu"];
+var topics = ["cat", "ronaldo","messi","hazard","zidane", "iniesta", "cafu"];
 
 // functions for displaying soccer data
 function displayButtons() {
@@ -17,7 +17,7 @@ for (var i =0; i < topics.length; i++){
     // dynamically generate button for all topics
     var Bottons = $("<button>");
      Bottons.addClass("soccerButtons");
-     Bottons.attr("data-name", topics[i]);
+     Bottons.attr("data-soccer", topics[i]);
      Bottons.text(topics[i]);
 
      $("#emptyPanel").append(Bottons);
@@ -48,39 +48,45 @@ var soccerStars = soccerName.split(" ").join("+");
 
 // construct the Giphy URL
 
-var queryURL =  "http://api.giphy.com/v1/gifs/search?q=" + soccerStars + "&api_key=dc6zaTOxFJmzC&limit=10";
+var queryURL =  "https://api.giphy.com/v1/gifs/search?q=" + soccerStars + "&rating=pg-13&limit=20&api_key=dc6zaTOxFJmzC";
+
+
 
 
 // make the Ajax call to the Giphy API
 
 
 $.ajax({
-  
-    url: queryURL,
+
     method: "GET",
+    url: queryURL
+
 }).then(function(response){
 
     var ratings = response.data;
 
     // display div for each of the element return 
 
-    for ( var i = 0; i < ratings.length; i++){
-        
-       // skip something
-      //  $("#soccerGifs").empty();
+      $("#soccerGifs").empty();
 
-        var pOne =("<div class = 'soccer'>");
+      for ( var i = 0; i < ratings.length; i++){
+        
+      
+
+        var pOne =("<div>");
+        pOne.addClass("soccerBottons");
         var pTwo = $("<p>").text("Rating: " + ratings[i].rating);
+
         
         pOne.append(pTwo);
 
        // creating an element to hold the image
-       var img = $("<img>");
-       image.attr("src", ratings[i].images.fixed_height_still.url);
-       image.attr("data-still", ratings[i].images.fixed_height_still.url);
-       image.attr("data-soccer", ratings[i].images.fixed_height.url);
-       image.attr("data-state", "still" );
-       pOne.append(image)
+       var imageId = $("<img>");
+       imageId.attr("src", ratings[i].images.fixed_height_still.url);
+       imageId.attr("data-still", ratings[i].images.fixed_height_still.url);
+       imageId.attr("data-soccer", ratings[i].images.fixed_height.url);
+       imageId.attr("data-state", "still" );
+       pOne.append(imageId);
       
     
          $("#soccerGifs").append(pOne);
@@ -98,7 +104,7 @@ $.ajax({
 
      if (state === "still") {
          $(this).find("img").attr("src", $(this).find("img").attr("data-animate"));
-         $(this).find("img").attr("data-state","animate");
+         $(this).find("img").attr("data-state", "animate");
 
 
      }  else {
@@ -117,10 +123,10 @@ $.ajax({
 
    // an event handler for soccer buttons to find appropriate gifs
 
-   $(document).on("click", "#soccerBottons", findSoccerGifs );
+   $(document).on("click", ".soccerBottons", findSoccerGifs );
 
    // event handler to make image animate 
-   $(document).on("click", "#animateBottons", findSoccerGifs );
+   $(document).on("click", ".animateBottons", animateAndStop );
 
 
 
